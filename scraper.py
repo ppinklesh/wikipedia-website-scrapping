f3ebea970272eb3b3d4a5f91e84495ec6f7efd08
pinklesh parjapati <pinkleshparjapati@gmail.com>
2021-03-02 11:45:40 +0530

    task completed in one go

---
+++ scraper.py
@@ -0,0 +1,48 @@
+'''
+install request, BeautifulSoup or bs4 and pandas library
+'''
+# imports and variables
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+csvData = {}
+serial_no = 0
+
+# url input for scrapping
+url = input('Please enter wikipedia URL: ')
+print('requesting to website data....')
+
+# url = 'https://en.wikipedia.org/wiki/Main_Page'
+r = requests.get(url)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+anchor = soup.find_all('a')
+
+print('looking for href data....')
+
+# loop for getting all of the href links
+for link in anchor:
+    data = str(link.get('href'))
+    serial_no+=1
+
+    if str(link.text) == '':
+        text = link.get('title')
+    else:
+        text = link.text
+
+    if data.startswith('/'):
+        hrefLink = 'https://en.wikipedia.org' + data
+    elif data.startswith('//'):
+        hrefLink = 'https:' + data
+    elif data.startswith('#'):
+        hrefLink = url+data
+    else:
+        hrefLink = data
+    
+    # storing data into csvData dictionary
+    csvData[serial_no] = [text,hrefLink]
+
+# writing data to csv file using pandas library
+data_df = pd.DataFrame.from_dict(csvData, orient='index', columns=['textContent', 'link'])
+data_df.to_csv('data.csv')
+print('scrapping done')
